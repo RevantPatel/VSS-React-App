70d23da0ad755645029f23dbaf7ae711677a5482
Revant Patel <revantpethani@gmail.com>
2025-09-25 17:55:27 +0530

    fix: Image Rendering

--- VSS_React/src/components/Card.jsx
+++ VSS_React/src/components/Card.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 
 const Card = ({ car }) => {
 
-    const imageUrl = car.imageUrl ? `${RENDER_URL}${car.imageUrl}` : '';
+    const imageUrl = car.imageUrl ? `${'https://vss-backend.onrender.com'}${car.imageUrl}` : '';
 
     return (
         <div>
