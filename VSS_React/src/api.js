67e55e44d3476c5c2f76e3a464b51c02a7fa65d6
Revant Patel <revantpethani@gmail.com>
2025-09-25 17:15:04 +0530

    Update API URL for production

--- VSS_React/src/api.js
+++ VSS_React/src/api.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
-const RENDER_URL = 'https://vss-backend.onrender.com';
-
 const API = axios.create({
-  baseURL: import.meta.env.MODE === 'production' 
-    ? RENDER_URL 
-    : 'http://localhost:5000',
+  baseURL: 'https://vss-backend.onrender.com',
 });
 
 export default API;
