25f8496ee1182d953032e25949ab084269565e34
Revant Patel <revantpethani@gmail.com>
2025-12-09 14:35:24 +0530

    Updated api

--- client/src/api.js
+++ client/src/api.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: import.meta.env.MODE === 'production'
-    ? ''
-    : 'http://localhost:5000',
+    ? 'https://vss-backend.onrender.com'
+    : '',
 });
 
 export default API;
 
 // https://vss-backend.onrender.com
+// http://localhost:5000
